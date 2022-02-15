a554b57f58e57896fb9caa36bc985633d89a784c
Elitedino <46204770+Elitedino@users.noreply.github.com>
2022-02-15 10:54:29 -0600

    semi-large update
    added main page button
    changed weather button to fade instead of collapse
    created printers.html
    changed magic.html
    started added navbar to other pages

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,16 @@
 console.log('hello, javascript runs');
+/*
+function clocktime(){
+    const curtime = new Date();
+    let ms = curtime.getMilliseconds();
+    let s = curtime.getSeconds();
+    let m = curtime.getMinutes();
+    let h = curtime.getHours();
+    let day = curtime.getDay();
+    let month = curtime.getMonth();
+    let year = curtime.getFullYear();
+    console.log(year + ";" + month + ";" + day + ";" + h + ";" + m + ";" + s + ";" + ms)
+    $('#clock').text(year + ";" + month + ";" + day + ";" + h + ";" + m + ";" + s + ";" + ms);
+    setTimeout(clocktime, 1);
+}
+*/
